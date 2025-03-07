c59b6e5d30cdb70bbb480487b278693ff83b36b8
39raisho1 <39raisho1@gmail.com>
2025-03-08 00:44:33 +0900

    初診・再診のページを正しいディレクトリに移動

---
+++ pages/saishin.js
@@ -0,0 +1,5 @@
+import { SaishinReservation } from "../components/ReservationForms";
+
+export default function SaishinPage() {
+  return <SaishinReservation />;
+}
---
+++ pages/shoshin.js
@@ -0,0 +1,5 @@
+import { ShoshinReservation } from "../components/ReservationForms";
+
+export default function ShoshinPage() {
+  return <ShoshinReservation />;
+}
